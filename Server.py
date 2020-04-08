965caa82d9e5a803d8c0ccf6bd061cb692852d86
R4z7 <50873749+R4z7@users.noreply.github.com>
2020-04-08 22:12:35 +0300

    Server startup

---
+++ Server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+def main():
+    app = Flask(__name__)
+    @app.route('/')
+    def index():
+        return "Hello world!"
+    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    main()
