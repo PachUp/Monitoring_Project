b59b8b250f592103d211f9c5d87cd56027696e05
R4z7 <50873749+R4z7@users.noreply.github.com>
2020-04-08 22:31:17 +0300

    Server db

--- Server.py
+++ Server.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import *
 
 
 def main():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users1.db'
+    db = SQLAlchemy(app)
+    class UserInfo(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        cpu_type = db.Column(db.TEXT)
+        ram_usage = db.Column(db.FLOAT)
+        mac_address = db.Column(db.TEXT)
+        running_processes = db.Column(db.TEXT)
+        cpu_usage_procentage = db.Column(db.FLOAT)
+        memory_usage_procentage = db.Column(db.FLOAT)
+
     @app.route('/')
     def index():
         return "Hello world!"
