e6eec2ce47b9e13be7c8eb5447f427bacaeeb3ea
R4z7 <50873749+R4z7@users.noreply.github.com>
2020-04-08 19:58:23 +0300

    Client#1 is ready

--- Client.py
+++ Client.py
@@ -3,23 +3,24 @@ import os
 import psutil
 import flask_restful
 import platform
-
+import getmac
+import requests
 
 class CpuDetails:
     def __init__(self):
         pass
     def utilization_procentage(self):
-        return "CPU usage percentage: " + str(psutil.cpu_percent(interval=5))
+        return psutil.cpu_percent(interval=5)
     def cpu_type(self):
-        return "CPU type: " + platform.processor()
+        return platform.processor()
 
 class MemoryDetails:
     def __init__(self):
         pass
     def ram_usage(self):
-        "Ram usage(in gb): " + str(psutil.virtual_memory().used/1000000000)
+        return psutil.virtual_memory().used/1000000000
     def memory_utilization_procentage(self):
-        return "Memory usage percentage: " + str(psutil.virtual_memory()[2])
+        return psutil.virtual_memory()[2]
 class ProcessDetails:
     def __init__(self):
         pass
@@ -30,8 +31,17 @@ class ProcessDetails:
         return running_programs
 
 
+def computer_mac_address():
+    return getmac.get_mac_address()
+
+
 def main():
-    pass
+    ProcessDetail = ProcessDetails()
+    CpuDetail = CpuDetails()
+    MemoryDetail = MemoryDetails()
+    print(MemoryDetail.ram_usage())
+    req = requests.post('http://127.0.0.1:5000', json={"running processes": ProcessDetail.get_running_processes(), "CPU type: ": CpuDetail.cpu_type(), "Ram usage: ":MemoryDetail.ram_usage()})
+    print(req.json())
 
 
 if __name__ == "__main__":
