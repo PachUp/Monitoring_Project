96781e8a3f57756ed936bfd36705115baffe6eeb
R4z7 <50873749+R4z7@users.noreply.github.com>
2020-04-08 16:35:43 +0300

    All client functions (Windows)

--- Client.py
+++ Client.py
@@ -1,13 +1,31 @@
 
 import os
 import psutil
+import flask_restful
+import platform
 
+
+class CpuDetails:
+    def utilization_procentage(self):
+        return "CPU usage percentage: " + str(psutil.cpu_percent(interval=5))
+    def cpu_type(self):
+        return "CPU type: " + platform.processor()
+
+class MemoryDetails:
+    def ram_usage(self):
+        "Ram usage(in gb): " + str(psutil.virtual_memory().used/1000000000)
+    def memory_utilization_procentage(self):
+        return "Memory usage percentage: " + str(psutil.virtual_memory()[2])
+class ProcessDetails:
+    def __init__(self):
+        pass
+    def get_running_processes(self):
+        running_programs = []
+        for running_program in psutil.process_iter():
+            running_programs.append(running_program.name())
+        return running_programs
 
 def main():
-    print("First code commit!")
-    while True:
-        print("CPU percentage: ", end="")
-        print(psutil.cpu_percent(interval=5))
 
 if __name__ == "__main__":
     main()
