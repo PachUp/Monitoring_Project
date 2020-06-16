da6e4dc9647ecb39851425dc53bc864677d12948
R4z7 <50873749+R4z7@users.noreply.github.com>
2020-06-17 00:22:49 +0300

    removed .js files - not working well as childen of the navbar.html

--- static/nevbar.js
+++
@@ -1,6 +0,0 @@
-$('.dropdown-nevbar').on('show.bs.dropdown', function() {
-    $(this).find('.dropdown-menu-nevbar').first().stop(true, true).slideDown();
-});
-  $('.dropdown-nevbar').on('hide.bs.dropdown', function() {
-    $(this).find('.dropdown-menu-nevbar').first().stop(true, true).slideUp();
-});
