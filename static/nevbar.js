933a4128265fd3efe3fbf24625666bc99df8b558
R4z7 <50873749+R4z7@users.noreply.github.com>
2020-06-16 23:39:16 +0300

    making all of the script tag into a .js file

---
+++ static/nevbar.js
@@ -0,0 +1,8 @@
+$('.dropdown-nevbar').on('show.bs.dropdown', function() {
+    $(this).find('.dropdown-menu-nevbar').first().stop(true, true).slideDown();
+  });
+
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+  $('.dropdown-nevbar').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu-nevbar').first().stop(true, true).slideUp();
+  });
